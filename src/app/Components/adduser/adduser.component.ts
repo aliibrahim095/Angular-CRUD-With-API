078662989b084be0d758e20aa1aca67e7b7a0f43
aliibrahim095 <4liebrahim@gmail.com>
2021-04-23 13:40:52 +0200

    add new user functionality

--- src/app/Components/adduser/adduser.component.ts
+++ src/app/Components/adduser/adduser.component.ts
@@ -1,16 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/Services/users.service';
 
 @Component({
   selector: 'app-adduser',
   templateUrl: './adduser.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AdduserComponent implements OnInit {
+  constructor(private myService: UsersService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  id: number = 4;
+  fname;
+  lname;
+  email;
+  age;
+  city;
+  street;
+
+  user: {
+    id: number;
+    fname: string;
+    lname: string;
+    email: string;
+    age: number;
+    address: {
+      city: string;
+      street: string;
+    }
+  };
+  AddNewUser() {
+    this.user = {
+      id: this.id,
+      fname: this.fname,
+      lname: this.lname,
+      email: this.email,
+      age: this.age,
+      address: { city: this.city, street: this.street }
+    };
+   this.myService.AddNewUser(this.user).subscribe();
+  //  console.log(this.user);
   }
-
 }
